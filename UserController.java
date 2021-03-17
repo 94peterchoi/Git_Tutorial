73d65fe41c39135cbf2da97a19967c8d19bf377a
peterchoi <choisang08@naver.com>
2021-03-17 11:09:00 +0900

    Add UserController [update]

--- UserController.java
+++ UserController.java
@@ -1,6 +1,5 @@
 @Controller
 public class UserController {
-    // 왜 안될까요 ..
     // 로그인 페이지로 이동
     @RequestMapping("/user/login.do")
     public String login() {
@@ -13,6 +12,14 @@ public class UserController {
     public String insert() {
         logger.info("경로 : insert");
         
+        return "user/insert";
+    }
+
+    // 회원수정 페이지로 이동
+    @RequestMapping("/user/update.do")
+    public String update() {
+        logger.info("어쩌구");
+
         return "user/insert";
     }
 
